7ecd245288ca2dd93018cce392fc248737a04254
J. Nick Koston <nick@koston.org>
2023-03-08 08:40:52 -1000

    lint

--- src/websockets/legacy/compatibility.py
+++ src/websockets/legacy/compatibility.py
@@ -25,7 +25,7 @@ else:  # pragma: no cover
 
 
 if sys.version_info[:2] < (3, 11):
-    from .async_timeout import timeout as asyncio_timeout
+    from .async_timeout import timeout as asyncio_timeout  # pragma: no cover
 else:  # pragma: no cover
     from asyncio import timeout as asyncio_timeout
 
